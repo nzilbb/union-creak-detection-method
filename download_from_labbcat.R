16319bb0a8cf11ad1016e4a303c14dffd35f6a77
Robert Fromont <robert@fromont.net.nz>
2023-01-19 16:15:35 -0300

    Updates to get create_phoneme_files.R and create_pm_files.R working with downloaded files.

--- download_from_labbcat.R
+++ download_from_labbcat.R
@@ -1,29 +1,62 @@
 ### This script downloads selected wav files and TextGrids from a LaBB-CAT corpus, and
 ### also creates a corresponding durations.txt file so you don't have to run
-### get_durations.praat 
+### get_durations.praat
+###
+### Make sure you change the settings below to suit your situation before execution.
+###
+### Packages that must be installed:
+###  - nzilbb.labbcat
+###  - rPraat
 ###
 ### You can run it from the command line using:
 ### `R --vanilla --slave < download_from_labbcat.R`
 ###
+### What to do after getting data from LaBB-CAT:
+### 1. Run create_phoneme_files.R
+### 1.1 Edit setwd path to match dir below
+### 1.2 Run create_phoneme_files.R
+### 2. Run create_pm_files.R
+### 2.1 Edit setwd path to match dir below
+### 2.2 Set chunked.files = FALSE
+### 2.3 Run create_pm_files.R
+### 3. Run get_am_output_sonorants.R
+### 3.1 Edit setwd path to match dir below
+### 3.2 The script assumes the SAMPA phoneme labels that are produced by WebMAUS.
+###     If LaBB-CAT uses a different phoneme set (it probably does!) then change the filter
+###     after line 38 to match your sonorant symbols.
+###     e.g. CELEX DISC sonorants are:
+###          c C E F H i I P q Q u U V 0 1 2 3 4 5 6 7 8 9 ~ # { $ @
+###          j l m n N r w 
+###
 ### Author: robert.fromont@canterbury.ac.nz
 ### Date: 2022-10-26
 ###
-### Change these setting to suit you own situation:
+
+####### Change these setting to suit you own situation: #######
+
 ### LaBB-CAT URL
 url <- "http://localhost:8080/labbcat/"
+
 ### Credentials for logging in to LaBB-CAT
 username <- "labbcat"
 password <- "labbcat" ## !!! DON'T COMMIT THE REAL PASSWORD TO GIT !!!
+
 ### An expression to identify which participants to process, e.g.
 ### - Participants in a specific corpus: "labels('corpus').includes('QB')"
 ### - Participant with a given gender: "first('participant_gender').label == 'NB'"
 ### - Both: "labels('corpus').includes('QB') && first('participant_gender').label == 'NB'"
-which.participants <- "/^AP5.*/.test(id)"
+which.participants <- "/^AP51[13].*/.test(id)"
+## (^AP51[12].* identifies just a couple of participants for testing purposes)
+
 ### The directory to download the files to
+### NB: this script deletes everything in here before doing anything else
 dir <- "data"
 
-#devtools::install_github("nzilbb/labbcat-R", "1.2-0")
+################## End of settings to update ##################
+
+devtools::install_github("nzilbb/labbcat-R", "1.2-0")
 library(nzilbb.labbcat)
+library(rPraat)
 
 ## Downloading wav files can be time consuming, so up the timeout to 5 minutes
 labbcatTimeout(300)
@@ -42,30 +75,53 @@ cat("\nGetting all transcripts for", length(participant.ids), "participants...")
 wav.files <- list()
 textgrid.files <- list()
 pm.files <- list()
+
+unlink(dir, recursive=TRUE)
+
+# create durations.txt on the fly so we don't have to run get_durations.praat
+durations.file <- file.path(dir, "durations.txt")
+if (file.exists(durations.file)) {
+    file.remove(durations.file)
+}
+
+# for each participant
 for (participant.id in participant.ids) {
+    cat("\n", participant.id, "...", sep = "")
     transcript.ids <- getTranscriptIdsWithParticipant(url, participant.id)
     for (transcript.id in transcript.ids) {
+        cat("\n ", transcript.id, "...", sep = "")
         ## get wav
         wav.file <- getMedia(url, transcript.id, "", "audio/wav", dir)
         
         if (!is.null(wav.file)) { # there is an audio file
-            ## rename wav to convert _ -> -
-            final.wav.file <- file.path(dir, gsub("_", "-", basename(wav.file)))
+            ## rename wav the way other scripts like it
+            final.wav.file <- file.path(
+                ## add 'file' number so create_pm_files.R works
+                dir, sub(".wav", "_1.wav",
+                         ## convert _ -> -
+                         gsub("_", "-", basename(wav.file)))) 
             file.rename(from=wav.file, to=final.wav.file)
 
-            cat(paste("\n", wav.file, "..."))
-            wav.files[[length(wav.files)+1]] <- wav.file
+            cat("\n", wav.file, " (", final.wav.file, ")...", sep = "")
+            wav.files[[length(wav.files)+1]] <- final.wav.file
             ## get TextGrid
             textgrid.file <- formatTranscript(
                 url, transcript.id, c("segment"), "text/praat-textgrid", dir)
             ## Rename the TextGrid the way the other scripts like it
-            ## i.e. some_name.TextGrid -> some-name_passage_pipeline.TextGrid
+            ## i.e. some_name.TextGrid -> some-name_pipeline.TextGrid
             final.textgrid.file <- file.path(
-                dir, sub(".TextGrid", "_passage_pipeline.TextGrid",
+                ## ${name}_1_pipeline.TextGrid so that create_phoneme_files.R works
+                dir, sub(".TextGrid", "_1_pipeline.TextGrid",
                          ## convert _ -> -
-                         gsub("_", "-", basename(textgrid.file))))
+                         gsub("_", "-", basename(textgrid.file)))) 
             file.rename(from=textgrid.file, to=final.textgrid.file)
             textgrid.files[[length(textgrid.files)+1]] <- final.textgrid.file
+
+            ## infer duration from TextGrid
+            textgrid <- tg.read(final.textgrid.file)
+            duration <- tg.getEndTime(textgrid)
+            write(paste(basename(final.wav.file), duration, sep="\t"),
+                  file=durations.file, append=TRUE)
             
             ## get .pm file
             pm.file <- getMedia(url, transcript.id, "", "application/pm", dir)
